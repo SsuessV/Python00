c042dbabc0c3ecca453e759d0700383d221f5c6e
Sue <ssuess.tv@gmail.com>
2026-08-18 22:12:04 +0200

    flake8 checked

--- ex6/ft_count_harvest_iterative.py
+++ ex6/ft_count_harvest_iterative.py
@@ -1,2 +1,5 @@
-def ft_seed_inventory(seed_type: str, quantity: int, unit: str)-> None:
-    
+def ft_count_harvest_iterative():
+    days = int(input("Days until harvest: "))
+    for i in range(1, days + 1):
+        print("Day", i)
+    print("Harvest time!")
